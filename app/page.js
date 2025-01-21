9fde8e4da0e0fe8358a96bf9563110e0d391cdd1
Priyabashu Ambedkar <priyabashukgg@gmail.com>
2025-01-21 13:10:37 +0530

    video update

--- app/page.js
+++ app/page.js
@@ -46,7 +46,7 @@ export default function Home() {
 
       <div className="text-white container mx-auto pb-32 pt-14 flex flex-col justify-center items-center">
         <h2 className="text-3xl font-bold text-center mb-14">Learn more about us</h2>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/QtaorVNAwbI?si=ZBRHmc4ECtcJF_41" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/7ODrsDrl3qk?si=Op-MFctapU6Dav9X" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
     </>
   );
